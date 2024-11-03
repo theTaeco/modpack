6a3d20c33e84126c80df0031065a0fb84a742a8a
Violet-Scarelli <alphaone150@gmail.com>
2024-11-02 21:19:28 -0700

    a bit of initial work on runic shards

--- kubejs/startup_scripts/startup.js
+++ kubejs/startup_scripts/startup.js
@@ -7,13 +7,15 @@ StartupEvents.registry('item', e => {
     .unstackable()
     .rarity('epic')
     .glow(true)
-    .useAnimation('spyglass')
+    .useAnimation('crossbow')
     .useDuration(_itemstack => 72000)
+    .barColor(_itemstack => Color.DARK_PURPLE)
+    .barWidth(_itemstack => 0)
     .use((_level, player, _hand) => {
       player.playSound("minecraft:block.respawn_anchor.charge", 5, 0);
       return true;
     })
-    .releaseUsing((_itemstack, _level, entity, _tick) => {
+    .releaseUsing((itemstack, _level, entity, _tick) => {
       var destination = entity.rayTrace(64, false);
       var destX = destination.getHitX();
       var destY = destination.getHitY();
@@ -31,6 +33,57 @@ StartupEvents.registry('item', e => {
         entity.playSound("minecraft:block.respawn_anchor.deplete", 5, 1);
       }
     });
+    e.create('inert_transport_shard')
+    .displayName("Inert Transport Shard")
+    .texture('runic_shards:item/transport_inert')
+    .unstackable()
+    .rarity("common")
+    .glow(false)
+    .tooltip("An inert runic shard, formatted for transport spells.");
+    e.create('inert_combat_shard')
+    .displayName("Inert Combat Shard")
+    .texture('runic_shards:item/combat_inert')
+    .unstackable()
+    .rarity("common")
+    .glow(false)
+    .tooltip("An inert runic shard, formatted for combat spells.");
+    e.create('inert_conjuring_shard')
+    .displayName("Inert Conjuring Shard")
+    .texture('runic_shards:item/conjuring_inert')
+    .unstackable()
+    .rarity("common")
+    .glow(false)
+    .tooltip("An inert runic shard, formatted for conjuring spells.");
+    e.create('inert_utility_shard')
+    .displayName("Inert Utility Shard")
+    .texture('runic_shards:item/utility_inert')
+    .unstackable()
+    .rarity("common")
+    .glow(false)
+    .tooltip("An inert runic shard, formatted for utility spells.");
+
+    e.create('basic_transport_shard')
+    .displayName("Basic Transport Shard")
+    .texture('runic_shards:item/transport_iron')
+    .unstackable()
+    .rarity("uncommon")
+    .glow(true)
+    .maxDamage(4)
+    .tooltip("A runic shard, formatted for transport spells.")
+    .use((_level, player, _hand) => {
+      var itemstack = player.mainHandItem;
+      var command = player.mainHandItem.nbt;
+      player.tell(command);
+      player.runCommandSilent("kubejs custom_command " +command);
+      itemstack.damageValue++;
+      if (itemstack.damageValue >= itemstack.maxDamage) {
+        itemstack.count = itemstack.count - 1;
+      }
+    })
+
     e.create('antenna').displayName('Antenna').texture('phonos:item/antenna');
     e.create('satellite_dish').displayName('Satellite Dish').texture('phonos:item/dish_antenna');
   })
+
+
+  
