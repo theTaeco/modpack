43af9ddeb030a5d6463593e008d89b0d8e1cee4f
Violet-Scarelli <alphaone150@gmail.com>
2024-11-04 20:15:12 -0800

    Arcane Arsenal, production version #1

--- kubejs/server_scripts/relics.js
+++ kubejs/server_scripts/relics.js
@@ -1,7 +1,12 @@
 ServerEvents.customCommand((event) => {
     var id = event.getId();
-
-    if (id = "blink"){
+    /**
+     * All custom spells go in here. Simply write your spell as a function, 
+     * then add the spell to the detection chain below this comment.
+     */
+    if (id == "detonate"){
+        detonate(event.getEntity(), event.getEntity().server);
+    } else if (id == "blink"){
         blink(event.getEntity(), event.getEntity().server);
     }
     /**
@@ -24,6 +29,18 @@ ServerEvents.customCommand((event) => {
     */
 
     function blink(entity, server){
+
         entity.runCommandSilent("tp ^ ^ ^5");
+    }
+    /**
+    * 
+    * @param {Internal.LivingEntity} entity
+    * @param {Internal.MinecraftServer} server
+    */
+    function detonate(entity, server){
+        var rayHit = entity.rayTrace(64, true);
+        if (rayHit != null)
+            var target = rayHit.entity;
+            target.level.explode(entity, target.getX(), target.getY(), target.getZ(), 10, false, "none");
     }
 })
